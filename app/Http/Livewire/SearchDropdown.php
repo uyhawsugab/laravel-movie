fb1653097ffa7e17cdfe62f953afb183d509faee
uywahsugab <wahyu_bagus_26rpl@student.smktelkom-mlg.sch.id>
2020-09-10 15:21:26 +0700

    search almost complete

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -9,19 +9,22 @@ use Illuminate\Support\Facades\Http;
 
 class SearchDropdown extends Component
 {
-    public $search = "hello there";
+    public $search = '';
     
     public function render()
     {
 
         $searchResults = [];
 
-        $searchResults = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/search/movie?query='.$this->search)
-            ->json()['results'];
+        if (strlen($this->search) > 2) {
+            $searchResults = Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/search/movie?query='.$this->search)
+                ->json()['results'];
+        }
+
         
         return view('livewire.search-dropdown', [
-            'searchResults' => $searchResults,
+            'searchResults' => collect($searchResults)->take(8),
         ]);
     }
 }
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -3,11 +3,19 @@
      <div class="absolute top-0">
         <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
     </div>
-    <div class="absolute bg-gray-800 rounded w-64 mt-4 text-sm">
-        <ul>
-            <li class="border-b border-gray-700">
-                <a href="" class="block hover:bg-gray-700 px-3 py-3">{{ $search }}</a>
-            </li>
-        </ul>
-    </div>
+    @if (strlen($search) > 2)
+        <div class="absolute bg-gray-800 rounded w-64 mt-4 text-sm">
+            @if ($searchResults->count() > 0)
+                <ul>
+                @foreach ($searchResults as $result)
+                    <li class="border-b border-gray-700">
+                        <a href="{{ route('movies.show',$result['id']) }}" class="block hover:bg-gray-700 px-3 py-3">{{ $result['title'] }}</a>
+                    </li>
+                @endforeach
+                </ul>
+            @else 
+                <div class="px-3 py-3">No Results for "{{ $search }}"</div>
+            @endif
+        </div> 
+    @endif
 </div>
