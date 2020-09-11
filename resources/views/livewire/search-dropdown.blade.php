c96be98cf8dcf33ea1a663bee8c43c9a08473f50
uywahsugab <wahyu_bagus_26rpl@student.smktelkom-mlg.sch.id>
2020-09-11 15:00:47 +0700

    making embed video for trailer

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,5 +1,5 @@
 <div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
-    <input wire:model.debounce.100ms="search" type="text" class="bg-gray-800 rounded-full w-64 px-4 py-1 pl-8 focus:outline-nfocus:shadow-outline" placeholder="Search" @focus="isOpen = true" @keydown.escape.window="isOpen = false" @keydown.shift.tab="isOpen = false">
+    <input wire:model.debounce.100ms="search" type="text" class="bg-gray-800 rounded-full w-64 px-4 py-1 pl-8 focus:outline-nfocus:shadow-outline" placeholder="Search" @focus="isOpen = true" @keydown.escape.window="isOpen = false" @keydown.shift.tab="isOpen = false" @keydown="isOpen = true">
      <div class="absolute top-0">
         <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
     </div>
@@ -12,7 +12,7 @@
                 <ul>
                 @foreach ($searchResults as $result)
                     <li class="border-b border-gray-700">
-                        <a href="{{ route('movies.show',$result['id']) }}" class="block hover:bg-gray-700 px-3 py-3 flex items-center" @if ($loop->last) @keydown.tab="isOpen =false" @endif>
+                        <a href="{{ route('movies.show',$result['id']) }}" class="block hover:bg-gray-700 px-3 py-3 flex items-center" @if ($loop->last) @keydown.tab.exact="isOpen =false" @endif>
                         @if ($result['poster_path'])        
                         <img class="w-8" src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster">
                         @else
